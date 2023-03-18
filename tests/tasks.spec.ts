f74a51024f15e6f629fbe2e7145f263b21d63710
thiagoluppi <thiago.luppi@icloud.com>
2023-03-18 10:41:11 +0000

    finalizando

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -2,12 +2,19 @@ import { test, expect } from '@playwright/test'
 import { faker } from '@faker-js/faker'
 
 
-test('Deve poder cadastrar uma nova tarefa @debug', async ({ page }) => {
+test('Deve poder cadastrar uma nova tarefa @debug @regression', async ({ page, request }) => {
+    const taskName = 'teste'
+
+    await request.delete(`http://localhost:3333/helper/tasks/${taskName}`)
+
     await page.goto('http://localhost:3000')
 
     const inputTaskName = page.locator('input[class*=InputNewTask]')
+
+
+    await inputTaskName.fill(taskName)
     // Usando o faker.lorem.paragraph ele acaba criando textos muitos grandes, então vamos mudar:
-    await inputTaskName.fill(faker.lorem.words())
+    // await inputTaskName.fill(faker.lorem.words())
 
     // Usando xpath:
     // await page.click('xpath=//button[contains(text(), "Create")]')
